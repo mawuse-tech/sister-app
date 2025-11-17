12de59b1b058213722963f01731c7c35f34a0a58
mawuse-tech <akpekemawuse1@gmail.com>
2025-11-17 14:35:11 +0000

    loading

--- src/helpers/ProtectRoute.jsx
+++ src/helpers/ProtectRoute.jsx
@@ -22,7 +22,13 @@ export const PrivateRoute = ({ children }) => {
 export const PublicRoute = ({ children }) => {
     const { loading, user } = useSelector((store) => store.isUserLoggedIn)
 
-    if (loading) return <div>loading ...</div>
+    if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    )
+  }
 
     return !user ? children : <Navigate to={'/userdash'} replace />
 
