278739ed505c782a70f8627a35f0211883dc2d14
mawuse-tech <akpekemawuse1@gmail.com>
2025-11-17 11:39:20 +0000

    error reset password

--- src/Pages/AuthPages/ResetPassword.jsx
+++ src/Pages/AuthPages/ResetPassword.jsx
@@ -9,6 +9,7 @@ const ResetPassword = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setEror] = useState(null)
 
   const navigate = useNavigate();
   const { token } = useParams();
